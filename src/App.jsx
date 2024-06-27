35416aca47a003fbfe0c8e0e400459187fda18b9
Athif Binu <binuathif@gmail.com>
2024-06-27 12:44:06 +0530

    addsence test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import Header from './Components/Header/Header';
-import Home from './Pages/Home';
-import Footer from './Components/Footer/Footer';
-import Copyright from './Components/Copyright';
-import Social from './Components/Social';
+import React from "react";
+// import Header from "./Components/Header/Header";
+// import Home from "./Pages/Home";
+// import Footer from "./Components/Footer/Footer";
+// import Copyright from "./Components/Copyright";
+// import Social from "./Components/Social";
 const App = () => {
-  return (
-    <>
-      <Social />
-      <Header/>
-      <Home/>
-      <Footer/>
-      <Copyright/>
-    </>
-  );
-}
+  return;
+  <div>
+    <div>
+      <div>
+        <button>order</button>
+        <button>profile</button>
+        <button>Adress</button>
+      </div>
+
+      <div>
+        <div>
+          <h1>Order Form</h1>
+        </div>
+
+        <div>
+          <h1>Profile Form</h1>
+        </div>
+
+        <div>
+          <h1>adress Form</h1>
+        </div>
+      </div>
+    </div>
+  </div>;
+};
 
 export default App;
+
+// <>
+//   <Social />
+//   <Header/>
+//   <Home/>
+//   <Footer/>
+//   <Copyright/>
+// </>
